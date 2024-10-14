06b65d59de8c7b9b30e755dfe9c78ef51821b77b
cristianmedac <cmm041@alu.medac.es>
2024-10-14 13:21:54 +0200

    PantallaSecundaria

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -8,51 +8,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Filtro vacantes',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Filtro vacantes'),
-        ),
-        body: Center(
-          child: DropdownExample(),
+      title: 'Stock Carpintería',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PrimeraPantalla(),
+    );
+  }
+}
+
+class PrimeraPantalla extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stock Carpintería'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navegar a la segunda pantalla
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SegundaPantalla()),
+            );
+          },
+          child: Text('Stock referencia'),
         ),
       ),
     );
   }
 }
 
-class DropdownExample extends StatefulWidget {
-  @override
-  _DropdownExampleState createState() => _DropdownExampleState();
-}
-
-class _DropdownExampleState extends State<DropdownExample> {
-  // Lista de opciones
-  List<String> _opciones = ['UGT', 'Comisiones Obreras'];
-
-  // Valor seleccionado inicialmente
-  String? _opcionSeleccionada;
-
+class SegundaPantalla extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    DropdownButton<String>(
-      value: _opcionSeleccionada,
-      items: _opciones.map((String opcion) {
-        return DropdownMenuItem<String>(
-          value: opcion,
-          child: Text(opcion, style: TextStyle(color: Colors.blue)),
-        );
-      }).toList(),
-      onChanged: (String? nuevoValor) {
-        setState(() {
-          _opcionSeleccionada = nuevoValor;
-        });
-      },
-      hint: Text('Seleccione una opción'),
-      icon: Icon(Icons.arrow_downward, color: Colors.blue),
-      underline: Container(height: 2, color: Colors.blue),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Buscar producto'),
+      ),
+      body: Center(
+        child: Text('Buscar producto/EAN'),
+      ),
     );
-
-    ;
   }
 }
