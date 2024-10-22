ef66ef7cdf3f1579eca45538b42531a9dcd04972
RogelioCB <rcb0005@alu.medac.es>
2024-10-22 16:47:32 +0200

    Creada la carpeta Pages con la pantalla de login. Cambiado en el main la línea 27 por home: LoginPage(),

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/firebase_options.dart';
+import 'pages/login.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DropdownButtonExample(),
+      home: LoginPage(),
     );
   }
 }
