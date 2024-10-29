11dfdae767e46a22b8d279ce625959bafd08fd67
cristianmedac <cmm041@alu.medac.es>
2024-10-29 11:47:13 +0100

    AddedIncMaterialButton

--- flutter_app/lib/pages/material_input_screen.dart
+++ flutter_app/lib/pages/material_input_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fancy_button_flutter/fancy_button_flutter.dart';
 import 'package:flutter/material.dart';
 
 class PantallaAddMaterial extends StatelessWidget {
@@ -7,10 +8,33 @@ class PantallaAddMaterial extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add Material'),
+        title: Text('Añadir Material'),
       ),
       body: Center(
-        child: Text('Add Material'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Introduce referencia, ubicacion y cantidad'),
+            SizedBox(height: 20),
+            FancyButton(
+              button_icon: Icons.add_circle_outline,
+              button_text: "Incrementar stock",
+              button_height: 40,
+              button_width: 300,
+              button_radius: 100,
+              button_color: Colors.lightBlue,
+              button_outline_color: Colors.black,
+              button_outline_width: 1,
+              button_text_color: Colors.white,
+              button_icon_color: Colors.white,
+              icon_size: 40,
+              button_text_size: 28,
+              onClick: () {
+                // Añadir función para introducir en la base de datos
+              },
+            )
+          ],
+        ),
       ),
     );
   }
