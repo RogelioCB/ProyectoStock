c05308e84687f5d2489c0ae08a929a1d61213714
RogelioCB <rcb0005@alu.medac.es>
2024-10-30 17:29:53 +0100

    Implementado el botón para ver la contraseña

--- flutter_app/lib/pages/login.dart
+++ flutter_app/lib/pages/login.dart
@@ -10,28 +10,20 @@ class LoginPage extends StatelessWidget {
 
   LoginPage({super.key});
 
- 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: AppBar(
-      //  title: Text('Login'),
-      //),
       backgroundColor: Colors.grey[300],
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            
             Icon(
               Icons.lock,
               size: 100,
             ),
-            SizedBox(
-              height: 50,
-            ),
+            SizedBox(height: 50),
             Text(
               'Welcome back!',
               style: TextStyle(
@@ -39,65 +31,34 @@ class LoginPage extends StatelessWidget {
                 fontSize: 16,
               ),
             ),
-            SizedBox(
-              height: 25,
-            ),
-            
-            MyTextfild( //email UI
+            SizedBox(height: 25),
+            MyTextfild(
               controller: _usernameController,
               labelText: 'Email',
               obscureText: false,
             ),
-
-
-            SizedBox(height: 10,),
-            
-            MyTextfildpassword(//Password UI
+            SizedBox(height: 10),
+            MyTextfildpassword(
               controller: _passwordController,
               labelText: 'Password',
-              obscureText: true,
+              // No necesitas especificar obscureText aquí
             ),
-
-            //Password UI
-            /*Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: TextField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey.shade400)),
-                    fillColor: Colors.grey.shade200,
-                    filled: true,
-                    labelText: 'Password'),
-                obscureText: true,
-              ),
-            ),*/
             SizedBox(height: 25),
             ElevatedButton(
-             
               onPressed: () async {
-                
                 String email = _usernameController.text.trim();
                 String password = _passwordController.text.trim();
- 
+
                 try {
-                  // Intenta iniciar sesión con Firebase
-                  // ignore: unused_local_variable
                   UserCredential userCredential = await FirebaseAuth.instance
                       .signInWithEmailAndPassword(
                           email: email, password: password);
 
-                  // Si el inicio de sesión es exitoso, navega a la pantalla principal
                   Navigator.pushReplacement(
                     context,
-                    MaterialPageRoute(
-                        builder: (context) => PantallaPrincipal()),
+                    MaterialPageRoute(builder: (context) => PantallaPrincipal()),
                   );
                 } catch (e) {
-                  // Muestra un mensaje de error si el inicio de sesión falla
                   ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(content: Text('Usuario o contraseña incorrectos')),
                   );
@@ -111,8 +72,12 @@ class LoginPage extends StatelessWidget {
               ),
               child: SizedBox(
                 width: double.infinity,
-                child: Text('Login', textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20) ,),),
+                child: Text(
+                  'Login',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
             ),
           ],
         ),
