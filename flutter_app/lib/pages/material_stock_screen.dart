23e5da7c57f7e9cacd07286b3855f1a3ee73092a
cristianmedac <cmm041@alu.medac.es>
2024-11-04 22:52:18 +0100

    Added SDK>23

--- flutter_app/lib/pages/material_stock_screen.dart
+++ flutter_app/lib/pages/material_stock_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fancy_button_flutter/fancy_button_flutter.dart';
 import 'package:flutter/material.dart';
 
 class PantallaStockProducto extends StatelessWidget {
@@ -7,10 +8,32 @@ class PantallaStockProducto extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Stock producto'),
+        title: Text('Buscar Stock Referencia/EAN'),
       ),
       body: Center(
-        child: Text('Agregar buscador'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Introduce Referencia/EAN'),
+            SizedBox(height: 20),
+            FancyButton(
+              button_text: "Buscar",
+              button_height: 40,
+              button_width: 300,
+              button_radius: 100,
+              button_color: Colors.lightBlue,
+              button_outline_color: Colors.black,
+              button_outline_width: 1,
+              button_text_color: Colors.white,
+              button_icon_color: Colors.white,
+              icon_size: 40,
+              button_text_size: 28,
+              onClick: () {
+                // Añadir función para introducir en la base de datos
+              },
+            )
+          ],
+        ),
       ),
     );
   }
